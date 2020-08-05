70274b728a5d9c8fa608df8f6acd2094624696ef
Vic Boudolf <vic@boudolf.com>
2020-08-04 21:55:52 -0400

    show full user name when viewing note

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :notes, dependent: :delete_all
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
