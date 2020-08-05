e9cf3af3e75d8e16c011a75aca43993ca097bef0
Vic Boudolf <vic@boudolf.com>
2020-08-05 00:14:00 -0400

    style note list

--- app/models/note.rb
+++ app/models/note.rb
@@ -6,9 +6,22 @@ class Note < ApplicationRecord
                      message: "body can't be blank if title is blank" }
   before_validation :default_title
 
+  def formatted_created_at
+    zone = ActiveSupport::TimeZone.new('Eastern Time (US & Canada)')
+    if(created_at >= 7.days.ago) then
+      date = created_at.in_time_zone(zone).strftime('%a')
+    else
+      date = created_at.in_time_zone(zone).strftime('%a, %b %d, %Y')
+    end
+
+    time = created_at.in_time_zone(zone).strftime('%I:%M%P')
+    "#{date} #{time}"
+  end
+
   private
 
   def default_title
     self.title = body[0..30] if title.blank?
   end
+
 end
