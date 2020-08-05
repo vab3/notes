bcdb6e3d5b8245b6fc8f56dd66e34ca690c229d7
Vic Boudolf <vic@boudolf.com>
2020-08-04 21:42:09 -0400

    make new note belong to current_user

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -26,7 +26,7 @@ class NotesController < ApplicationController
 
   # GET /notes/new
   def new
-    @note = Note.new
+    @note = current_user.notes.new
   end
 
   # GET /notes/1/edit
@@ -36,7 +36,7 @@ class NotesController < ApplicationController
   # POST /notes
   # POST /notes.json
   def create
-    @note = Note.new(note_params)
+    @note = current_user.notes.create(note_params)
 
     respond_to do |format|
       if @note.save
