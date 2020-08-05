00e2bb4474d09e5f90a68e97ec09d08ae473ccd0
Vic Boudolf <vic@boudolf.com>
2020-08-04 21:18:34 -0400

    add validations for Note

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,3 +1,14 @@
 class Note < ApplicationRecord
   belongs_to :user
+  validates :title, length: { maximum: 30 }
+  validates :body, length: { maximum: 1000 },
+                   presence: { if: -> { title.blank? },
+                     message: "body can't be blank if title is blank" }
+  before_validation :default_title
+
+  private
+
+  def default_title
+    self.title = body[0..30] if title.blank?
+  end
 end
